7b8ba358b67c9ccbf6cac46cfc48c3c04b96caea
Yash Gupta <gyash24x7@gmail.com>
2023-05-10 17:15:35 +0530

    setup tests for app

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { deserializeUser, handleAuthCallback, handleGetLoggedInUser, handleLogout, requireUser } from "@s2h/auth";
-import { literatureExpressHandler } from "@s2h/literature/router";
+import { literatureExpressHandler, literatureTrpcPanelHandler } from "@s2h/literature/router";
 import type { IEnhancedLitGame } from "@s2h/literature/utils";
 import { Publisher } from "@s2h/utils";
 import cookieParser from "cookie-parser";
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer( app );
 const io = new Server( server, {
 	cors: {
-		origin: [ "http://localhost:5173" ],
+		origin: [ "http://localhost:3000" ],
 		allowedHeaders: [ "Authorization" ],
 		credentials: true
 	}
@@ -29,7 +29,7 @@ const io = new Server( server, {
 app.use( morgan( "tiny" ) );
 app.use( cookieParser() );
 app.use( express.json() );
-app.use( cors( { credentials: true, origin: "http://localhost:5173" } ) );
+app.use( cors( { credentials: true, origin: "http://localhost:3000" } ) );
 app.use( deserializeUser( prisma ) );
 
 const literatureNameSpace = io.of( "/literature" );
@@ -57,6 +57,8 @@ app.get( "/api/me", requireUser( prisma ), handleGetLoggedInUser() );
 app.delete( "/api/auth/logout", requireUser( prisma ), handleLogout() );
 
 app.get( "/api/auth/callback/google", handleAuthCallback( prisma ) );
+
+app.get( "/api/literature/docs", literatureTrpcPanelHandler( "http://localhost:8000/api/literature" ) );
 
 app.use(
 	"/api/literature",
