9f5ad24b55ddc87b3772cab08208c7197d13d6ed
Yash Gupta <gyash24x7@gmail.com>
2023-06-07 18:52:31 +0530

    added literature service

--- apps/backend/scripts/seed.mjs
+++ apps/backend/scripts/seed.mjs
@@ -1,43 +1,43 @@
-import {r} from "rethinkdb-ts";
+import { r } from "rethinkdb-ts";
 
 const userData = [
-    {
-        name: "Swarnava Das",
-        email: "swarnava@gmail.com",
-        avatar: "https://avatars.dicebear.com/api/micah/bcdjkvbdjkjbdnvkjdbkbvjdk.svg?r=50",
-        salt: "bcdjkvbdjkbvjdk"
-    },
-    {
-        name: "Preetam Sekhar",
-        email: "preetam@gmail.com",
-        avatar: "https://avatars.dicebear.com/api/micah/jkbdjdbchjjhfbjvscbkb.svg?r=50",
-        salt: "jkbdjdbchjscbkb"
-    },
-    {
-        name: "Vijayabharathi Murugan",
-        email: "bj@gmail.com",
-        avatar: "https://avatars.dicebear.com/api/micah/qrrwtgvdhjckskjdnvkjdlkjd.svg?r=50",
-        salt: "qrrwtgvdhjckslkjd"
-    },
-    {
-        name: "Pratik Manghwani",
-        email: "pratik@gmail.com",
-        avatar: "https://avatars.dicebear.com/api/micah/mxnbcnmbwhidvbdkjnkldjxbcj.svg?r=50",
-        salt: "mxnbcnmbwhikldjxbcj"
-    }
+	{
+		name: "Swarnava Das",
+		email: "swarnava@gmail.com",
+		avatar: "https://avatars.dicebear.com/api/micah/bcdjkvbdjkjbdnvkjdbkbvjdk.svg?r=50",
+		salt: "bcdjkvbdjkbvjdk"
+	},
+	{
+		name: "Preetam Sekhar",
+		email: "preetam@gmail.com",
+		avatar: "https://avatars.dicebear.com/api/micah/jkbdjdbchjjhfbjvscbkb.svg?r=50",
+		salt: "jkbdjdbchjscbkb"
+	},
+	{
+		name: "Vijayabharathi Murugan",
+		email: "bj@gmail.com",
+		avatar: "https://avatars.dicebear.com/api/micah/qrrwtgvdhjckskjdnvkjdlkjd.svg?r=50",
+		salt: "qrrwtgvdhjckslkjd"
+	},
+	{
+		name: "Pratik Manghwani",
+		email: "pratik@gmail.com",
+		avatar: "https://avatars.dicebear.com/api/micah/mxnbcnmbwhidvbdkjnkldjxbcj.svg?r=50",
+		salt: "mxnbcnmbwhikldjxbcj"
+	}
 ];
 
 async function seed() {
-    const connection = await r.connect({host: "personal.local", port: 28015});
-    const db = r.db("stairway");
+	const connection = await r.connect( { host: "personal.local", port: 28015 } );
+	const db = r.db( "stairway" );
 
-    const res = await db.table("users").insert(userData).run(connection)
-        .catch(err => console.log(err));
+	const res = await db.table( "users" ).insert( userData ).run( connection )
+		.catch( err => console.log( err ) );
 
-    console.log(res);
+	console.log( res );
 
-    return () => connection.close();
+	return () => connection.close();
 }
 
 
-seed().catch(err => console.log(err)).then(async closeConnection => await closeConnection());
+seed().catch( err => console.log( err ) ).then( async closeConnection => await closeConnection() );
