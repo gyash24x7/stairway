e1a1c99f99970fdd9ee375f4147833e097d2137e
Yash Gupta <gyash24x7@gmail.com>
2023-10-19 23:01:40 +0530

    fix auth-core

--- packages/auth/core/tests/services/jwt.service.test.ts
+++ packages/auth/core/tests/services/jwt.service.test.ts
@@ -1,6 +1,9 @@
-import { describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import type { AppConfig } from "@s2h/core";
 import { JwtService } from "../../src/services";
+import * as jose from "jose";
+import { unlink, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 
 describe( "JwtService", () => {
 
@@ -21,6 +24,19 @@ describe( "JwtService", () => {
 			port: 8000
 		}
 	};
+
+	beforeAll( async () => {
+		const { privateKey, publicKey } = await jose.generateKeyPair( "RS256" );
+		await writeFile(
+			join( __dirname, mockConfig.auth.privateKeyPath ),
+			await jose.exportPKCS8( privateKey )
+		);
+
+		await writeFile(
+			join( __dirname, mockConfig.auth.publicKeyPath ),
+			await jose.exportSPKI( publicKey )
+		);
+	} );
 
 	it( "should throw error if invalid token", async () => {
 		const jwtService = new JwtService( mockConfig );
@@ -50,4 +66,10 @@ describe( "JwtService", () => {
 		expect( authInfo?.verified ).toBe( true );
 		expect( authInfo?.avatar ).toBe( "avatar" );
 	} );
+
+	afterAll( async () => {
+		await unlink( join( __dirname, mockConfig.auth.publicKeyPath ) );
+		await unlink( join( __dirname, mockConfig.auth.privateKeyPath ) );
+	} );
+
 } );
