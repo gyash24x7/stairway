e69ed0c78b30da0498cb6d0ea1d508fd4962aaea
Yash Gupta <gyash24x7@gmail.com>
2023-11-29 11:01:59 +0530

    fix build issues

--- packages/literature/store/src/client.ts
+++ packages/literature/store/src/client.ts
@@ -13,9 +13,11 @@ import type {
 	TransferMove,
 	TransferTurnInput
 } from "@literature/types";
-import superagent from "superagent";
+import ky from "ky";
 
 const BASE_URL = "http://localhost:8000/api/literature";
+
+type GameDataResponse = { gameData: GameData, playerData: PlayerSpecificData };
 
 export class Paths {
 	public static readonly JOIN_GAME = "/join";
@@ -31,70 +33,47 @@ export class LiteratureClient {
 
 	async createGame( data: CreateGameInput ) {
 		const createGamePath = BASE_URL.concat( "" );
-		return superagent.post( createGamePath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as GameData );
+		return ky.post( createGamePath, { json: data, credentials: "include" } ).json<GameData>();
 	}
 
 	async joinGame( data: JoinGameInput ) {
 		const joinGamePath = BASE_URL.concat( Paths.JOIN_GAME );
-		return superagent.post( joinGamePath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as GameData );
+		return ky.post( joinGamePath, { json: data, credentials: "include" } ).json<GameData>();
 	}
 
 	async addBots( { gameId }: { gameId: string } ) {
 		const addBotsPath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.ADD_BOTS );
-		return superagent.put( addBotsPath )
-			.send( {} )
-			.withCredentials()
-			.then( res => res.body as PlayerData );
+		return ky.put( addBotsPath, { credentials: "include" } ).json<PlayerData>();
 	}
 
 	async createTeams( { gameId, ...data }: CreateTeamsInput & { gameId: string } ) {
 		const createTeamsPath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.CREATE_TEAMS );
-		return superagent.put( createTeamsPath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as TeamData );
+		return ky.put( createTeamsPath, { json: data, credentials: "include" } ).json<TeamData>();
 	}
 
 	async startGame( { gameId }: { gameId: string } ) {
 		const startGamePath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.START_GAME );
-		await superagent.put( startGamePath ).withCredentials();
+		await ky.put( startGamePath, { credentials: "include" } ).json<TeamData>();
 	}
 
 	async askCard( { gameId, ...data }: AskCardInput & { gameId: string } ) {
 		const askCardPath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.ASK_CARD );
-		return superagent.put( askCardPath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as AskMove );
+		return ky.put( askCardPath, { json: data, credentials: "include" } ).json<AskMove>();
 	}
 
 	async callSet( { gameId, ...data }: CallSetInput & { gameId: string } ) {
 		const callSetPath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.CALL_SET );
-		return superagent.put( callSetPath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as CallMove );
+		return ky.put( callSetPath, { json: data, credentials: "include" } ).json<CallMove>();
 	}
 
 	async transferTurn( { gameId, ...data }: TransferTurnInput & { gameId: string } ) {
 		const transferTurnPath = BASE_URL.concat( "/" ).concat( gameId ).concat( Paths.TRANSFER_TURN );
-		return superagent.put( transferTurnPath )
-			.send( data )
-			.withCredentials()
-			.then( res => res.body as TransferMove );
+		return ky.put( transferTurnPath, { json: data, credentials: "include" } ).json<TransferMove>();
 	}
 
 	async loadGameData( { gameId }: { gameId: string } ) {
 		const getGameDataPath = BASE_URL.concat( "/" ).concat( gameId );
-		return superagent.get( getGameDataPath )
-			.withCredentials()
-			.then( res => res.body as { gameData: GameData, playerData: PlayerSpecificData } );
+		return ky.put( getGameDataPath, { credentials: "include" } ).json<GameDataResponse>();
 	}
 }
 
