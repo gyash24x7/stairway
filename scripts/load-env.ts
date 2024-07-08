84f2237976b1632230b6c1b873ab7433f004fa56
Yash Gupta <38939232+gyash24x7@users.noreply.github.com>
2024-07-08 19:45:58 +0530

    Updated env load script for frontend

--- scripts/load-env.ts
+++ scripts/load-env.ts
@@ -1,7 +1,10 @@
 let envFileContent = "";
 
-Object.keys(Bun.env).map(key => {
-	envFileContent += `${key}=${Bun.env[key]}\n`
-})
+Object.keys( Bun.env ).map( key => {
+	envFileContent += `${ key }=${ Bun.env[ key ] }\n`
+} )
 
-await Bun.write("apps/backend/.env", envFileContent);
+const isBackend = process.argv[ 2 ] === "backend"
+console.log( `Loading env vars for ${ process.argv[ 2 ] }` )
+
+await Bun.write( isBackend ? "apps/backend/.env" : "apps/stairway/.env", envFileContent );
