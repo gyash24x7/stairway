1d4f5d0f22d0acf89a9a760170a9f193e99ed16a
Yash Gupta <gyash24x7@gmail.com>
2024-09-22 18:22:05 +0530

    Allowed railway healthcheck endpoint and added load ev script

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -29,8 +29,9 @@ const app = await NestFactory.create( AppModule );
 const host = Bun.env[ "HOST" ] ?? "localhost";
 const port = Bun.env[ "PORT" ] ?? "8000";
 const FRONTEND_URL = Bun.env[ "FRONTEND_URL" ] ?? "http://localhost:3000";
+const RAILWAY_HEALTHCHECK_URL = "healthcheck.railway.app";
 
-app.enableCors( { origin: FRONTEND_URL, credentials: true } );
+app.enableCors( { origin: [ FRONTEND_URL, RAILWAY_HEALTHCHECK_URL ], credentials: true } );
 app.setGlobalPrefix( "api" );
 app.use( cookieParser() );
 
