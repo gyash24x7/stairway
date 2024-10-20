f7a3384b7994e37fef6987208f5ed9df716592b8
Yash Gupta <38939232+gyash24x7@users.noreply.github.com>
2024-10-21 00:28:01 +0530

    Update publisher.ts

--- packages/api/utils/src/publisher.ts
+++ packages/api/utils/src/publisher.ts
@@ -7,34 +7,28 @@ const logger = createLogger( "Publisher" );
 const publisher = redis.createClient( {
 	url: process.env[ "REDIS_URL" ] ?? "redis://localhost:6379"
 } );
-await publisher.connect();
+
+function publishEvent( event: string, payload: string ) {
+	publisher.connect().then( () => {
+		publisher.publish( event, payload ).then( () => {
+			logger.debug( format( "Published Event: %s Payload: %s", event, payload ) );
+		} );
+	} );
+}
+
 
 export function publishLiteraturePlayerEvent( gameId: string, playerId: string, event: string, data: any ) {
-	const eventKey = `${ playerId }:${ event }`;
-	publisher.publish( "literature-event", JSON.stringify( { gameId, playerId, event, data } ) ).then( () => {
-		logger.debug( format( "Published Literature Direct Message:", eventKey ) );
-	} );
+	publishEvent( "literature-event", JSON.stringify( { gameId, playerId, event, data } ) );
 }
 
 export function publishLiteratureGameEvent( gameId: string, event: string, data: any ) {
-	const eventKey = `${ gameId }:${ event }`;
-
-	publisher.publish( "literature-event", JSON.stringify( { gameId, event, data } ) ).then( () => {
-		logger.debug( format( "Published Literature Room Message:", eventKey ) );
-	} );
+	publishEvent( "literature-event", JSON.stringify( { gameId, event, data } ) );
 }
 
 export function publishCallbreakPlayerEvent( gameId: string, playerId: string, event: string, data: any ) {
-	const eventKey = `${ playerId }:${ event }`;
-	publisher.publish( "literature-event", JSON.stringify( { gameId, playerId, event, data } ) ).then( () => {
-		logger.debug( format( "Published Literature Direct Message:", eventKey ) );
-	} );
+	publishEvent( "callbreak-event", JSON.stringify( { gameId, playerId, event, data } ) );
 }
 
 export function publishCallbreakGameEvent( gameId: string, event: string, data: any ) {
-	const eventKey = `${ gameId }:${ event }`;
-
-	publisher.publish( "literature-event", JSON.stringify( { gameId, event, data } ) ).then( () => {
-		logger.debug( format( "Published Literature Room Message:", eventKey ) );
-	} );
+	publishEvent( "callbreak-event", JSON.stringify( { gameId, event, data } ) );
 }
