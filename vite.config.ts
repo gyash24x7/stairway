875828dc1612da1bba105886da4c004cba297118
Yash Gupta <gyash24x7@gmail.com>
2025-06-27 08:49:11 +0530

    Upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { redwood } from "rwsdk/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig( {
-	environments: {
-		ssr: {}
-	},
 	plugins: [
+		cloudflare( {
+			viteEnvironment: { name: "worker" }
+		} ),
 		redwood(),
 		tailwindcss()
 	]
