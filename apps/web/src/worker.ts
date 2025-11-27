aa37c70ab9a1ef1d955d091e2cc24db9374c6958
Yash Gupta <gyash24x7@gmail.com>
2025-11-27 19:05:22 +0530

    added origin env var

--- apps/web/src/worker.ts
+++ apps/web/src/worker.ts
@@ -14,7 +14,7 @@ export default {
 		const url = new URL( request.url );
 		const services = {
 			session: new SessionService( env.SESSION_KV, env.AUTH_SECRET_KEY ),
-			auth: new AuthService( env.DB, env.WEBAUTHN_RP_ID, env.WEBAUTHN_KV ),
+			auth: new AuthService( env.DB, env.WEBAUTHN_RP_ID, env.WEBAUTHN_ORIGIN, env.WEBAUTHN_KV ),
 			callbreak: new CallbreakService( env.CALLBREAK_DO, env.CALLBREAK_KV ),
 			fish: new FishService( env.FISH_DO, env.FISH_KV ),
 			wordle: new WordleService( env.WORDLE_KV )
