75240de18f3fec4fbd4827c97ded3ab53707d810
Yash Gupta <gyash24x7@gmail.com>
2025-11-27 10:35:33 +0530

    reset for monorepo

--- types/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-import type { HibernationPluginContext } from "@orpc/server/hibernation";
-import type { ResponseHeadersPluginContext } from "@orpc/server/plugins";
-import type { Session } from "../src/auth/types";
-
-declare global {
-	type Ctx = ResponseHeadersPluginContext & HibernationPluginContext & { session?: Session }
-}
